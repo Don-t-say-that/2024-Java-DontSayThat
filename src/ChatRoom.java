dcc68d0272c14582c03b22f5fa21a025a8397b4a
hyewon4052 <s2311@e-mirim.hs.kr>
2024-12-05 23:18:00 +0900

    ✨[feat] : 금칙어

--- src/ChatRoom.java
+++ src/ChatRoom.java
@@ -44,6 +44,10 @@ public class ChatRoom extends JFrame {
             Socket socket = new Socket("localhost", 8000);
             out = new PrintWriter(socket.getOutputStream(), true);
 
+            // 서버로 사용자 이름과 금칙어 전송
+            out.println(username);
+            out.println("/forbidden " + JOptionPane.showInputDialog(this, "상대방의 금칙어를 입력하세요:"));
+
             // 서버로 메시지 읽기
             new Thread(() -> {
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
@@ -72,4 +76,5 @@ public class ChatRoom extends JFrame {
             inputField.setText("");
         }
     }
+
 }
